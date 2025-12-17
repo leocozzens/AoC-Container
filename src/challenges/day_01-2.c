391de3c699b483d7904727aead543cef7e55812c
Leo Cozzens <cozzensleo@gmail.com>
2025-12-17 15:49:38 -0600

    Added day one part 2 completed challenge code

---
+++ src/challenges/day_01-2.c
@@ -0,0 +1,40 @@
+// C standard headers
+#include <stdlib.h>
+#include <stdbool.h>
+// Local headers
+#include <challenge.h>
+#include <feed.h>
+
+const char *dataFileName = "./data/day_01.txt";
+
+ErrorData evaluate(InputData *input, Answer *result) {
+    *result = (Answer) { "Password", 0 };
+    int position = 50;
+
+    LineFeed feed = create_linefeed(input);
+    for(char *line = get_linefeed(&feed); line != NULL; line = get_linefeed(&feed)) {
+        int rotations, overflows = 0;
+        switch(*line) {
+            case 'L':
+                rotations = atoi(line + 1);
+                int start = position;
+                position -= rotations;
+                if(position <= 0) {
+                    overflows = (-position / 100) + 1;
+                    if(start == 0) overflows--;
+                }
+                position = (position % 100 + 100) % 100;
+                break;
+            case 'R':
+                rotations = atoi(line + 1);
+                position += rotations;
+                overflows = position / 100;
+                position %= 100;
+                break;
+            default:
+                continue;
+        }
+        result->output += overflows;
+    }
+    return emptySuccess;
+}
