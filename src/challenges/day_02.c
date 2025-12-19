4afa26bbeb1fade9de773c70dd5b9445636aa0ba
Leo Cozzens <cozzensleo@gmail.com>
2025-12-19 12:55:46 -0600

    Stubbed out day_02 source file

--- src/challenges/day_02.c
+++ src/challenges/day_02.c
@@ -8,8 +8,6 @@ const char delimiter = ',';
 const char *dataFileName = "./data/day_02.txt";
 
 ErrorData evaluate(InputData *input, Answer *result) {
-    *result = (Answer) { "Password", 0 };
-
     LineFeed feed = create_linefeed(input);
     for(char *line = get_linefeed(&feed); line != NULL; line = get_linefeed(&feed)) {
         // PROCESS HERE
