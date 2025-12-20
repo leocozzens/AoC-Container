53dcf70f7525abd4663e0cc7257b17ad3ed1e4c7
Leo Cozzens <cozzensleo@gmail.com>
2025-12-19 22:26:07 -0600

    Completed day 3

--- src/challenges/day_03.c
+++ src/challenges/day_03.c
@@ -1,14 +1,54 @@
+// C standard headers
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 // Local headers
 #include <challenge.h>
 #include <feed.h>
 
+#define NUMBER_DIGITS 2
+
 const char delimiter = '\n';
 const char *dataFileName = "./data/day_03.txt";
 
+static char *find_highest_digit(char *line, size_t length, bool first);
+
 ErrorData evaluate(InputData *input, Answer *result) {
+    *result = (Answer) { "Total output joltage", 0 };
+
     LineFeed feed = create_linefeed(input);
     for(char *line = get_linefeed(&feed); line != NULL; line = get_linefeed(&feed)) {
-        // PROCESS HERE
+        char numberText[NUMBER_DIGITS + 1];
+        for(int i = 0; i < NUMBER_DIGITS; i++) {
+            line = find_highest_digit(line, strlen(line), i == 0);
+            if(line == NULL)
+                return CONSTRUCT_ERROR(
+                    CHALLENGE_ENCODING_FAILURE,
+                    "Invalid input string"
+                );
+            numberText[i] = *line;
+            if(!isdigit(*line))
+                return CONSTRUCT_ERROR(
+                    CHALLENGE_ENCODING_FAILURE,
+                    "Invalid input string"
+                );
+            line++;
+        }
+        result->output += atoi(numberText);
     }
     return emptySuccess;
+}
+
+static char *find_highest_digit(char *line, size_t length, bool first) {
+    if(length == 0) return NULL;
+    if(first) length--;
+    char *highest = line;
+    if(length == 1)
+        return highest;
+    for(size_t i = 1; i < length; i++) {
+        if(line[i] > *highest)
+            highest = line + i;
+    }
+    return highest;
 }
